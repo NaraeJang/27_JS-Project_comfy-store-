9232670688cd3fecf9cb6a40dd6c8a028c9cce70
NaraeJang <j.naaaarae@gmail.com>
2023-08-09 18:45:22 -0400

    Set up HTML

---
+++ src/toggleSidebar.js
@@ -0,0 +1,5 @@
+import { getElement } from "./utils.js";
+
+const toggleNav = getElement(".toggle-nav"),
+  sidebarOverlay = getElement(".sidebar-overlay"),
+  closeBtn = getElement(".sidebar-close");
