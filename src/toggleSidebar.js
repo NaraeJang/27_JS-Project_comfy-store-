f53357f3298dda6d425c0f65ee307cec9e236799
NaraeJang <j.naaaarae@gmail.com>
2023-08-09 18:47:59 -0400

    update toggleSidebar.js

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -3,3 +3,11 @@ import { getElement } from "./utils.js";
 const toggleNav = getElement(".toggle-nav"),
   sidebarOverlay = getElement(".sidebar-overlay"),
   closeBtn = getElement(".sidebar-close");
+
+toggleNav.addEventListener("click", () => {
+  sidebarOverlay.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebarOverlay.classList.remove("show");
+});
