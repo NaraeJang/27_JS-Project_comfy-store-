84f6133bd4bd55b55674517c53437858459d96bd
NaraeJang <j.naaaarae@gmail.com>
2023-08-13 14:27:59 -0400

    setup addToCart

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -32,9 +32,31 @@ export const addToCart = (id) => {
     // update values
   }
 
+  // add one to the item count
+  displayCartItemCount();
+  // display Cart totals
+  displayCartTotal();
+  // set cart in the storage
+  setStorageItem("cart", cart);
   openCart();
 };
 
+function displayCartItemCount() {
+  const amount = cart.reduce((total, cartItem) => {
+    return (total += cartItem.amount);
+  }, 0);
+  cartItemCountDOM.textContent = amount;
+}
+
+function displayCartTotal() {
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+
+  cartTotalDOM.textContent = `Total: ${formatPrice(total)}`;
+}
+
+// function displayCartTotal() {}
 const init = () => {
   console.log(cart);
 };
