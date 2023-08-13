ebff5cdd316c5bd4eeb37d34378680e589c60e7f
NaraeJang <j.naaaarae@gmail.com>
2023-08-13 16:26:23 -0400

    update items

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -30,6 +30,10 @@ export const addToCart = (id) => {
     console.log(cart);
   } else {
     // update values
+    const amount = increaseAmount(id);
+    const items = [...cartItemsDOM.querySelectorAll(".cart-item-amount")];
+    const newAmount = items.find((value) => value.dataset.id === id);
+    newAmount.textContent = amount;
   }
 
   // add one to the item count
@@ -60,6 +64,20 @@ function displayCartItemsDOM() {
   cart.forEach((cartItem) => {
     addToCartDOM(cartItem);
   });
+}
+
+function increaseAmount(id) {
+  let newAmount;
+
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1;
+      cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+    }
+    return cartItem;
+  });
+
+  return newAmount;
 }
 function setupCartFunctionality() {}
 
